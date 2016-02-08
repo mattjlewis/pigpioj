aa09c65cd3d2bfb3fc495572c21fe4fd2b18b28b
Matt Lewis <matthew.lewis@btinternet.com>
2016-02-08 20:22:32 +0000

    Added I2C and SPI support

--- pigpioj-java/src/main/java/com/diozero/pigpioj/PigpioI2C.java
+++ pigpioj-java/src/main/java/com/diozero/pigpioj/PigpioI2C.java
@@ -31,7 +31,8 @@ import java.io.IOException;
 
 public class PigpioI2C {
 	/** This returns a handle for the device at the address on the I2C bus
-	 * For the SMBus commands the low level transactions are shown at the end of the function description. The following abbreviations are used.
+	 * For the SMBus commands the low level transactions are shown at the end of the function description.
+	 * The following abbreviations are used.
 	 * S      (1 bit) : Start bit
 	 * P      (1 bit) : Stop bit
 	 * Rd/Wr  (1 bit) : Read/Write bit. Rd equals 1, Wr equals 0.
@@ -45,5 +46,41 @@ public class PigpioI2C {
 	 * [..]: Data sent by the device. */
 	public static native int i2cOpen(int i2cBus, int i2cAddr, int i2cFlags) throws IOException;
 	/** This closes the I2C device associated with the handle */
-	public static native int i2cClose(int handle) throws IOException;
+	public static native void i2cClose(int handle) throws IOException;
+	/** This reads a single byte from the device associated with handle */
+	public static native int i2cReadByte(int handle) throws IOException;
+	/** This sends a single byte to the device associated with handle */
+	public static native void i2cWriteByte(int handle, int bVal) throws IOException;
+	/** This reads a single byte from the specified register of the device associated with handle */
+	public static native int i2cReadByteData(int handle, int i2cReg) throws IOException;
+	/** This writes a single byte to the specified register of the device associated with handle */
+	public static native void i2cWriteByteData(int handle, int i2cReg, int bVal) throws IOException;
+	/** This reads a single 16 bit word from the specified register of the device associated with handle */
+	public static native int i2cReadWordData(int handle, int i2cReg) throws IOException;
+	/** This writes a single 16 bit word to the specified register of the device associated with handle */
+	public static native void i2cWriteWordData(int handle, int i2cReg, int wVal) throws IOException;
+	/** This writes 16 bits of data to the specified register of the device associated with handle and reads 16 bits of data in return */
+	public static native int i2cProcessCall(int handle, int i2cReg, int wVal) throws IOException;
+	/**
+	 * This reads a block of up to 32 bytes from the specified register of the device associated with handle.
+	 * The amount of returned data is set by the device
+	 * @return Returns the number of bytes read (>0) if OK
+	 */
+	public static native int i2cReadBlockData(int handle, int i2cReg, byte[] buf) throws IOException;
+	/** This writes up to 32 bytes to the specified register of the device associated with handle */
+	public static native void i2cWriteBlockData(int handle, int i2cReg, byte[] buf, int count) throws IOException;
+	/**
+	 * This reads count bytes from the specified register of the device associated with handle
+	 * @param count The count may be 1-32
+	 * @return Returns the number of bytes read (>0) if OK
+	 */
+	public static native int i2cReadI2CBlockData(int handle, int i2cReg, byte[] buf, int count) throws IOException;
+	/** This writes 1 to 32 bytes to the specified register of the device associated with handle */
+	public static native void i2cWriteI2CBlockData(int handle, int i2cReg, byte[] buf, int count) throws IOException;
+	/** This reads count bytes from the raw device into buffer. Returns count (>0) if OK */
+	public static native int i2cReadDevice(int handle, byte[] buffer, int count) throws IOException;
+	/** This writes count bytes from buffer to the raw device */
+	public static native void i2cWriteDevice(int handle, byte[] buffer, int count) throws IOException;
+	/** This sets the I2C (i2c-bcm2708) module "use combined transactions" parameter on or off */
+	public static native void i2cSwitchCombined(boolean setting) throws IOException;
 }
---
+++ pigpioj-java/src/main/java/com/diozero/pigpioj/PigpioSPI.java
@@ -0,0 +1,59 @@
+package com.diozero.pigpioj;
+
+import java.io.IOException;
+
+public class PigpioSPI {
+	/**
+	 * This function returns a handle for the SPI device on the channel. Data will be
+	 * transferred at baud bits per second. The flags may be used to modify the default
+	 * behaviour of 4-wire operation, mode 0, active low chip select.
+	 * An auxiliary SPI device is available on the A+/B+/Pi2/Zero and may be selected
+	 * by setting the A bit in the flags. The auxiliary device has 3 chip selects and
+	 * a selectable word size in bits.
+	 * spiFlags consists of the least significant 22 bits.
+	 * 
+	 * 21 20 19 18 17 16 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+	 *  b  b  b  b  b  b  R  T  n  n  n  n  W  A u2 u1 u0 p2 p1 p0  m  m
+	 * mm defines the SPI mode.
+	 * Warning: modes 1 and 3 do not appear to work on the auxiliary device.
+	 * Mode POL PHA
+	 *  0    0   0
+	 *  1    0   1
+	 *  2    1   0
+	 *  3    1   1
+	 * px is 0 if CEx is active low (default) and 1 for active high.
+	 * ux is 0 if the CEx gpio is reserved for SPI (default) and 1 otherwise.
+	 * A is 0 for the standard SPI device, 1 for the auxiliary SPI. The auxiliary device
+	 * is only present on the A+/B+/Pi2/Zero.
+	 * W is 0 if the device is not 3-wire, 1 if the device is 3-wire. Standard SPI device only.
+	 * nnnn defines the number of bytes (0-15) to write before switching the MOSI line
+	 * to MISO to read data. This field is ignored if W is not set. Standard SPI device only.
+	 * T is 1 if the least significant bit is transmitted on MOSI first, the default (0)
+	 * shifts the most significant bit out first. Auxiliary SPI device only.
+	 * R is 1 if the least significant bit is received on MISO first, the default (0) receives
+	 * the most significant bit first. Auxiliary SPI device only.
+	 * bbbbbb defines the word size in bits (0-32).
+	 * The default (0) sets 8 bits per word. Auxiliary SPI device only.
+	 * The other bits in flags should be set to zero
+	 */
+	public static native int spiOpen(int spiChan, int baud, int spiFlags) throws IOException;
+	/** This functions closes the SPI device identified by the handle */
+	public static native void spiClose(int handle) throws IOException;
+	/**
+	 * This function reads count bytes of data from the SPI device associated with the handle
+	 * @return Returns the number of bytes transferred if OK
+	 */
+	public static native int spiRead(int handle, byte[] buf, int count) throws IOException;
+	/**
+	 * This function writes count bytes of data from buf to the SPI device associated with the handle
+	 * @return Returns the number of bytes transferred if OK
+	 */
+	public static native int spiWrite(int handle, byte[] buf, int count) throws IOException;
+	/**
+	 * This function transfers count bytes of data from txBuf to the SPI device associated
+	 * with the handle. Simultaneously count bytes of data are read from the device and
+	 * placed in rxBuf
+	 * @return Returns the number of bytes transferred if OK
+	 */
+	public static native int spiXfer(int handle, byte[] txBuf, byte[] rxBuf, int count) throws IOException;
+}
